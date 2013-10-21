48ade7630f413f7442e2f0e400ea60eeab40c8cc
Josh <git@joshglazebrook.com>
2013-10-21 03:42:52 -0700

    updated example

--- examples/bind.js
+++ examples/bind.js
@@ -23,5 +23,8 @@ SocksFactory.createConnection(options, function(err, socket, info) {
         // BIND request completed, now a tcp client should connect to this endpoint:
         console.log(info);
         // { port: 3334, host: '202.101.228.108' }
+
+        // Resume! You need to!
+        socket.resume();
     }
 });
